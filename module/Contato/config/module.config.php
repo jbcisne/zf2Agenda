aaa33fac8da903aea31e89f583c728df62835b89
juliano.buzanello <juliano.buzanello@BSB-SIGMA0506.sigma.com.br>
2014-03-20 14:49:48 -0300

    Adicionando fluxos do crud

--- module/Contato/config/module.config.php
+++ module/Contato/config/module.config.php
@@ -3,7 +3,8 @@ return array(
     # definir e gerenciar controllers
     'controllers' => array(
         'invokables' => array(
-            'HomeController' => 'Contato\Controller\HomeController'
+            'HomeController' => 'Contato\Controller\HomeController',
+            'ContatosController' => 'Contato\Controller\ContatosController',
         ),
     ),
 
@@ -16,6 +17,31 @@ return array(
                     'route'    => '/',
                     'defaults' => array(
                         'controller' => 'HomeController',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            # literal para action sobre home
+            'sobre' => array(
+                'type'      => 'Literal',
+                'options'   => array(
+                    'route'    => '/sobre',
+                    'defaults' => array(
+                        'controller' => 'HomeController',
+                        'action'     => 'sobre',
+                    ),
+                ),
+            ),
+            'contatos' => array(
+                'type'      => 'Segment',
+                'options'   => array(
+                    'route'    => '/contatos[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'ContatosController',
                         'action'     => 'index',
                     ),
                 ),
